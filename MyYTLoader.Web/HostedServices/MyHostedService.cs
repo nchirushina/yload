73eef91c0e789ddc0b02a4ac58792ea77e70e969
Наталья Чирушина <n.chirushina@datareon.ru>
2025-03-07 22:09:48 +0300

    Заменила IHostedService на BackgroundService

--- MyYTLoader.Web/HostedServices/MyHostedService.cs
+++ MyYTLoader.Web/HostedServices/MyHostedService.cs
@@ -1,9 +1,10 @@
 ﻿
 using MyYTLoader.Domain;
+using System.Threading;
 
 namespace MyYTLoader.Web.HostedServices
 {
-    public class MyHostedService : IHostedService
+    public class MyHostedService : BackgroundService
     {
         private readonly ILogsProvider _logsProvider;
         private Task? _doWorkTask;
@@ -13,21 +14,9 @@ namespace MyYTLoader.Web.HostedServices
             _logsProvider = logsProvider;
         }
 
-        public async Task StartAsync(CancellationToken cancellationToken)
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _doWorkTask = Task.Run(() => DoWork(cancellationToken));
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            Task.WaitAll(_doWorkTask);
-            Console.WriteLine("Мы остановили MyHostedService");
-            return Task.CompletedTask;
-        }
-
-        private async Task DoWork(CancellationToken cancellationToken)
-        {
-            while (!cancellationToken.IsCancellationRequested)
+            while (!stoppingToken.IsCancellationRequested)
             {
                 _logsProvider.AddLog(DateTime.UtcNow.ToString());
                 await Task.Delay(1000);
